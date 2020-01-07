85e95bf37dabde03f73f0598ac76cb28b2a02d93
nsenecal <neilsenecalwork@gmail.com>
2020-01-06 23:28:45 -0700

    Media Bar Added
    Allows for scrubbing through media.
    Now capaple of loading unique files for each video.
    New bug(s) introduced.

--- filereadtest.py
+++
@@ -1,8 +0,0 @@
-#current video time = total elapsed time + current time - the time the vieo was started/unpaused
-result = dict()
-with open("Test.txt") as file:
-    for line in file:
-        time, duration = line.split(",")
-        result[int(time)] = int(duration.strip("\n"))
-print(list(result)[2])
-print(result[list(result)[2]])
